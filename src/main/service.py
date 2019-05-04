617485649024e1f0017eca73f27fe82ed89f9c11
Paul M <djpauldaniel@gmail.com>
2019-05-04 15:38:20 -0700

    Updated directory structure. Initial implementation of dictionary filtering

---
+++ src/main/service.py
@@ -0,0 +1,42 @@
+
+
+# input: list of dictionaries, list of key:value pair queries as dictionaries
+# output: the input list, filtered by the queries.
+# example: 
+#    input:
+#       users: [{"name": "root", "uid": 0 },{"name": "dwoodlins", "uid": 1001}, {"name": "dwoodlins", "uid": 1002}]
+#       queries: [{"name": "dwoodlins"}, {"uid": 1002}]
+#    output: 
+#       [{'name': 'dwoodlins', 'uid': 1002}]
+def filterResults(users, queries):
+
+    # if no query parameters passed in, return the input users. 
+    if queries == []:
+        return users
+
+    unfiltered_list = users
+
+    for query in queries:
+        # successively filter the input dictionary by each query parameter,
+        # until we have a list of items matching all the queries. 
+        filtered_list = []
+        query_key = list(query)[0]
+        query_value = query[query_key]
+
+        for user in unfiltered_list:
+            if user[query_key] == query_value:
+                filtered_list.append(user)
+
+        unfiltered_list = filtered_list    
+        
+        # quit early if no items matched this query parameter. 
+        if filtered_list == []:
+            break 
+            
+    return filtered_list
+
+
+users = [{"name": "root", "uid": 0 },{"name": "dwoodlins", "uid": 1001}, {"name": "dwoodlins", "uid": 1002}]
+queries = [{"name": "dwoodlins"}, {"uid": 1002}]
+print(filterResults(users,queries))
+# expect {"name": "dwoodlins", "uid": 1002}
