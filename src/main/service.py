1dd3ab8c267788753a56e3041f8d5bae16b93bee
Paul M <djpauldaniel@gmail.com>
2019-05-12 15:46:44 -0700

    Implemented users and users/query API calls, and passwd parsing method.

--- src/main/service.py
+++ src/main/service.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+import os
+import csv
 
 app = Flask(__name__)
 
@@ -10,7 +13,7 @@ app = Flask(__name__)
 #       queries: [{"name": "dwoodlins"}, {"uid": 1002}]
 #    output: 
 #       [{'name': 'dwoodlins', 'uid': 1002}]
-def filterResults(users, queries):
+def filterResults(users, queries = []):
 
     # if no query parameters passed in, return the input users. 
     if queries == []:
@@ -29,13 +32,79 @@ def filterResults(users, queries):
             if user[query_key] == query_value:
                 filtered_list.append(user)
 
-        unfiltered_list = filtered_list    
+        unfiltered_list = filtered_list
         
         # quit early if no items matched this query parameter. 
         if filtered_list == []:
-            break 
+            break
             
     return filtered_list
+
+
+# parseFileToDict
+# input:  file path to a 
+# output: dictionary of key:value pairs corresponding to the lines in the files. 
+#def parseFileToDict(file_path, column_names):
+#    pass
+
+# input: optional env variable PASSWDFILE_PATH, the path to the passwd file. if not present, defaults to /etc/passwd 
+# output: dictionary of key:value pairs, corresponding to the entries in the passwd file. 
+def getUsersDict():
+    default_passwd_path = "/etc/passwd"
+    optional_configured_path = os.environ.get('PASSWDFILE_PATH')
+    passwd_path = ""
+
+    if optional_configured_path is not None:
+        # we can also check if this path exists, and fall back on the default. 
+        passwd_path = optional_configured_path
+    else:
+        passwd_path = default_passwd_path
+    passwd_fieldnames = ["user",
+                         "password",
+                         "uid",
+                         "gid",
+                         "comment",
+                         "home",
+                         "shell"]
+
+    # csv reader creates an ordered dict. we just want a regular dict.
+    ordered_dict_users = []
+    with open(passwd_path, mode='r', newline='' ) as f:
+        reader = csv.DictReader(f, delimiter=':', quoting=csv.QUOTE_NONE, fieldnames = passwd_fieldnames)
+        ordered_dict_users = list(reader)
+
+    # convert ordered dicts to regular dicts. 
+    dict_users = []
+    for item in ordered_dict_users:
+        dict_users.append(dict(item))
+
+    return dict_users
+    
+    # alternatively use string.split on each line of the passwd file. 
+    # with open(passwd_path, "r", newline ="\n") as f:
+    #     passwd_text = f.read().splitlines()
+
+    # output = []
+    # for line in passwd_text:
+    #     output.append(line.split(':'))
+
+    # output_dictlist = [{}]*len(output)
+
+    # for line in range(0,len(output)):
+    #     for item in range(0,len(passwd_fieldnames)):
+    #         # create a dictionary that maps the field names to their values, per line
+    #         output_dictlist[line][passwd_fieldnames[item]] = output[line][item]
+
+    # return output
+
+# GROUPFILE_PATH
+def getGroupsDict():
+
+    # if entry["users"] is not None and "," in entry["users"]
+    #   entry["users"] = entry["users"].split()
+    pass
+
+
 
 # temporary test for filterResults
 @app.route('/filterResultTest')
@@ -44,3 +113,33 @@ def runFilterResultTest():
     queries = [{"name": "dwoodlins"}, {"uid": 1002}]
     # expect {"name": "dwoodlins", "uid": 1002}
     return str(filterResults(users,queries))
+
+@app.route('/passwdParseTest')
+def runPasswdParseTest():
+    return str(getUsersDict())
+
+
+def getAllUsers(users):
+    return filterResults(users)
+
+@app.route('/users')
+def getUsers():
+    users = getUsersDict()
+    #return str([request.args.to_dict()])
+    return str(getAllUsers(users))
+
+@app.route('/users/query')
+def getQueriedUsers():
+    users = getUsersDict()
+
+    if users is None:
+        #404 or other error: could not find passwd file or passwd file blank.
+        pass
+
+    query = []
+    for item in request.args:
+        # check the query parameters to make sure the requested fields exist
+        if item in users[0].keys():
+            query.append({item: request.args.get(item)})
+
+    return str(filterResults(users,query))
